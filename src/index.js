0024fa20dfa0fe6d75dc02cc70cf0766957ff04f
AnnaMarandola <annamarandola@gmail.com>
2021-06-29 17:33:14 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+} from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import '@fontsource/roboto';
+
+let theme =createMuiTheme({
+typography: {
+  useNextVariants: true,
+  fontFamily: "Roboto",
+  h1: {
+    color: "black",
+    fontSize: "4rem",
+    '@media (max-width:600px)': {
+      fontSize: "3rem",
+    },
+  }
+},
+
+overrides: {
+  MuiCssBaseline: {
+    "@global": {
+      "@font-face": "Roboto",
+    },
+  } 
+},
+
+palette: {
+  primary: {
+    main: "#34313D",
+    deepBlue: "#3A3054",
+    seaBlue: "#4B3F6B",
+    grey: "#9E9AA8",
+    turquoise: "#2BD0D0",
+    whiteish: "#EFF1F7",
+  },
+},
+
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MuiThemeProvider theme={theme}>
+  <CssBaseline />
+  <App />
+</MuiThemeProvider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
